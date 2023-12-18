62b63ee51c02f41e841f4d30b589604fb3084180
Nick Davies <git@nicolasdavies.com.au>
2023-12-17 17:18:14 -0700

    17 Switch to A* and it's now ~500ms for part 1

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use std::cell::RefCell;
-use std::collections::VecDeque;
+use std::collections::BinaryHeap;
 use std::rc::Rc;
 
 advent_of_code::solution!(17);
@@ -44,14 +44,11 @@ impl Direction {
 type Grid<T> = Vec<Vec<T>>;
 
 #[derive(Debug)]
-struct Map(Grid<u32>);
-
-#[derive(Debug, Clone, Ord, Eq, PartialEq, PartialOrd, Hash)]
-struct Location(usize, usize);
+struct Map(Grid<usize>);
 
 impl Map {
-    fn get(&self, location: &Location) -> &u32 {
-        &self.0[location.0][location.1]
+    fn get(&self, location: &Location) -> usize {
+        self.0[location.0][location.1]
     }
 
     fn get_location(&self, x: usize, y: usize) -> Option<Location> {
@@ -59,6 +56,10 @@ impl Map {
             .get(x)
             .and_then(|row| row.get(y))
             .map(|_| Location(x, y))
+    }
+
+    fn bottom_right(&self) -> Location {
+        Location(self.0.len() - 1, self.0[self.0.len() - 1].len() - 1)
     }
 
     fn go_direction(&self, current: &Location, direction: &Direction) -> Option<Location> {
@@ -83,6 +84,15 @@ impl Map {
     }
 }
 
+#[derive(Debug, Clone, Ord, Eq, PartialEq, PartialOrd, Hash)]
+struct Location(usize, usize);
+
+impl Location {
+    fn manhattan_dist(&self, other: &Self) -> usize {
+        self.0.abs_diff(other.0) + self.1.abs_diff(other.1)
+    }
+}
+
 #[derive(Clone, Debug)]
 struct Movement<'a> {
     map: &'a Map,
@@ -91,10 +101,34 @@ struct Movement<'a> {
     min_distance: usize,
     max_distance: usize,
     current_distance: usize,
-    total_cost: u32,
+    total_cost: usize,
     // We cache by the same vec shape as the input map
     // and also for each input direction.
-    best: Rc<RefCell<Grid<[Option<u32>; 4]>>>,
+    best: Rc<RefCell<Grid<[Option<usize>; 4]>>>,
+}
+
+impl PartialEq for Movement<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.weight() == other.weight()
+    }
+}
+impl Eq for Movement<'_> {}
+
+impl PartialOrd for Movement<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Movement<'_> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let target = self.map.bottom_right();
+
+        let my_weight = self.total_cost + self.location.manhattan_dist(&target);
+        let other_weight = other.total_cost + other.location.manhattan_dist(&target);
+
+        my_weight.cmp(&other_weight).reverse()
+    }
 }
 
 impl<'a> Movement<'a> {
@@ -123,6 +157,11 @@ impl<'a> Movement<'a> {
             total_cost: 0,
             best: Rc::new(RefCell::new(cache)),
         }
+    }
+
+    fn weight(&self) -> usize {
+        let target = self.map.bottom_right();
+        self.total_cost + self.location.manhattan_dist(&target)
     }
 
     fn test_path(&self, new_direction: Direction) -> Option<Self> {
@@ -195,23 +234,18 @@ impl<'a> Movement<'a> {
     }
 }
 
-fn seek_end(input: &str, min_distance: usize, max_distance: usize) -> Result<Option<u32>> {
+fn seek_end(input: &str, min_distance: usize, max_distance: usize) -> Result<Option<usize>> {
     let map = parse_input(input).context("Failed to parse input")?;
 
-    let mut costs: Vec<Vec<Option<u32>>> = Vec::new();
-    for row in &map.0 {
-        costs.push(vec![None; row.len()]);
-    }
-
-    let mut to_visit = VecDeque::new();
-    to_visit.push_front(Movement::new(
+    let mut to_visit = BinaryHeap::new();
+    to_visit.push(Movement::new(
         &map,
         map.get_location(0, 0).context("Expected to find (0,0)")?,
         Direction::East,
         min_distance,
         max_distance,
     ));
-    to_visit.push_front(Movement::new(
+    to_visit.push(Movement::new(
         &map,
         map.get_location(0, 0).context("Expected to find (0,0)")?,
         Direction::South,
@@ -224,7 +258,7 @@ fn seek_end(input: &str, min_distance: usize, max_distance: usize) -> Result<Opt
         .context("Expected to find bottom right")?;
     let mut best: Option<Movement> = None;
     while !to_visit.is_empty() {
-        let node = to_visit.pop_front().unwrap();
+        let node = to_visit.pop().unwrap();
         for next_node in node.available_paths() {
             if next_node.location == target && next_node.current_distance >= min_distance {
                 match &best {
@@ -238,7 +272,7 @@ fn seek_end(input: &str, min_distance: usize, max_distance: usize) -> Result<Opt
                     }
                 }
             }
-            to_visit.push_back(next_node);
+            to_visit.push(next_node);
         }
     }
     Ok(best.map(|n| n.total_cost))
@@ -249,18 +283,18 @@ fn parse_input(input: &str) -> Result<Map> {
     for line in input.lines() {
         let mut out_line = Vec::new();
         for char in line.chars() {
-            out_line.push(char.to_digit(10).context("Failed to parse input digit")?);
+            out_line.push(char.to_digit(10).context("Failed to parse input digit")? as usize);
         }
         out.push(out_line);
     }
     Ok(Map(out))
 }
 
-pub fn part_one(input: &str) -> Result<Option<u32>, anyhow::Error> {
+pub fn part_one(input: &str) -> Result<Option<usize>, anyhow::Error> {
     seek_end(input, 0, 3)
 }
 
-pub fn part_two(input: &str) -> Result<Option<u32>, anyhow::Error> {
+pub fn part_two(input: &str) -> Result<Option<usize>, anyhow::Error> {
     seek_end(input, 4, 10)
 }
 
