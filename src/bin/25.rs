e381d740b2b96818af592a0fbc0abf85538d60e8
Nick Davies <git@nicolasdavies.com.au>
2023-12-27 15:52:09 -0700

    Added solution to day 25

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use petgraph::graphmap::UnGraphMap;
+use std::collections::BTreeSet;
 
 advent_of_code::solution!(25);
 
@@ -22,15 +23,41 @@ fn parse_input(input: &str) -> Result<UnGraphMap<&str, ()>> {
     Ok(out)
 }
 
-pub fn part_one(input: &str) -> Result<Option<u32>, anyhow::Error> {
+fn external_connections(
+    graph: &UnGraphMap<&str, ()>,
+    node_set: &BTreeSet<&str>,
+    target: &str,
+) -> usize {
+    let mut external = 0;
+    for (_, edge, _) in graph.edges_directed(target, petgraph::Direction::Outgoing) {
+        if !node_set.contains(edge) {
+            external += 1;
+        }
+    }
+    external
+}
+
+pub fn part_one(input: &str) -> Result<Option<usize>, anyhow::Error> {
     let graph = parse_input(input).context("Failed to parse input")?;
-    println!("{:?}", graph);
-    std::fs::write(
-        "graph",
-        format!("{:?}", petgraph::dot::Dot::with_config(&graph, &[])),
-    )
-    .context("Failed to write graph to file")?;
-    Ok(None)
+
+    let mut lhs: BTreeSet<&str> = graph.nodes().collect();
+    let mut external_nodes = 0;
+
+    while external_nodes != 3 {
+        let (_, highest_external) = lhs
+            .iter()
+            .map(|n| (external_connections(&graph, &lhs, n), n))
+            .max()
+            .unwrap();
+        lhs.remove(*highest_external);
+
+        external_nodes = lhs
+            .iter()
+            .map(|n| external_connections(&graph, &lhs, n))
+            .sum();
+    }
+
+    Ok(Some(lhs.len() * (graph.node_count() - lhs.len())))
 }
 
 pub fn part_two(_input: &str) -> Result<Option<u32>, anyhow::Error> {
@@ -46,14 +73,6 @@ mod tests {
         let input = &advent_of_code::template::read_file_part("examples", DAY, 1);
         let result = part_one(input)?;
         assert_eq!(result, Some(54));
-        Ok(())
-    }
-
-    #[test]
-    fn test_part_two() -> anyhow::Result<()> {
-        let input = &advent_of_code::template::read_file_part("examples", DAY, 2);
-        let result = part_two(input)?;
-        assert_eq!(result, None);
         Ok(())
     }
 }
